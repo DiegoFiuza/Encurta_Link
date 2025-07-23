fd1a214728290a7488520c0a12cacbb07c2e0b63
DiegoFiuza <nasserdiegof@gmail.com>
2025-07-23 10:53:39 -0300

    Init: Encurtador Criado

---
+++ src/index.js
@@ -0,0 +1,33 @@
+'use strict'
+const link = document.querySelector('#link');
+const botao = document.querySelector('#enviar');
+const container = document.querySelector('.output');
+const verifierHTTP = 'https://www.';
+
+botao.addEventListener('click', ()=>{
+    container.innerHTML = '';  //Limpando container
+    const encurtado = document.createElement('a');
+    encurtado.target = '__blank';
+    const envio = document.createElement('p');
+    let rand = (length)=> Math.random().toString(36).substring(2,2 + length);
+    if(!link.value.includes(verifierHTTP)){
+        const newLink = `${verifierHTTP}${link.value}`
+        encurtado.href = newLink
+        encurtado.textContent= `shorter.url/${rand(4)}`;
+        encurtado.classList.add('text-blue-600');
+        encurtado.classList.add('underline')
+    }
+    else{
+        encurtado.href = link.value
+        encurtado.textContent= `shorter.url/${rand(4)}`;
+        encurtado.classList.add('text-blue-600');
+        encurtado.classList.add('underline')
+    }
+    if(encurtado.textContent !== ''){
+        envio.appendChild(encurtado);
+        container.appendChild(envio);
+        container.classList.remove('hidden');
+        link.value = '';
+    }
+
+})
